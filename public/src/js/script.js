22a0e87363c6e1d3324fc5898a7b2a204b9f23b6
GuliMaster <43322103+GuliMaster@users.noreply.github.com>
2020-10-10 23:42:09 +0300

    Add files via upload

---
+++ public/src/js/script.js
@@ -0,0 +1,47 @@
+const server = "http://192.168.1.128:80";
+const xhr = new XMLHttpRequest();
+
+function EncodeRequest(text) {
+    if (!text) {
+        return document.getElementById('status').textContent = 'Введите сообщение! Пожалуйста... ♥';
+    }
+    xhr.open('POST', server + '/encode', true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(`str=${text.toLowerCase()}`);
+    xhr.onload = () => {
+        let response = xhr.response.substring(1, (xhr.response.length-1));
+        let responseText = response.substring(1, (response.indexOf(',') - 1));
+        let responseKey = response.substring(response.indexOf(',') + 2, (response.length - 1));
+        document.getElementById('textarea').value = responseText;
+        document.getElementById('key').style.display = 'flex';
+        document.getElementById('key').value = responseKey;
+    }
+    xhr.onerror = () => {
+        document.getElementById('status').textContent = 'Ошибка соединения... Т_Т';
+    }
+    xhr.onprogress = (e) => {
+        document.getElementById('status').textContent = `Загрузка... ${e.loaded} из ${e.total}`;
+    }
+}
+function DecodeRequest(text, key) {
+    if (!key) {
+        return document.getElementById('status').textContent = 'Введите ключ! Пожалуйста... ♥';
+    }
+    if (!text) {
+        return document.getElementById('status').textContent = 'Введите сообщение! Пожалуйста... ♥';
+    }
+    xhr.open('POST', server + '/decode', true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.send(`str=${text.toLowerCase()}&key=${key}`);
+    xhr.onload = () => {
+        document.getElementById('textarea').value = xhr.response;
+    }
+    xhr.onerror = () => {
+        document.getElementById('status').textContent = 'Ошибка соединения... Т_Т';
+    }
+    xhr.onprogress = (e) => {
+        document.getElementById('status').textContent = `Загрузка... ${e.loaded} из ${e.total}`;
+    }
+}
+
+
